610148b44a0e8554f633c2241a987b8b369fe020
jrizza88 <jetorres88@gmail.com>
2019-08-11 15:50:40 -0400

    completed file_io and added some own code and files not instructed from assignment

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,10 +9,34 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
-
+f = open('foo.txt', 'r')
+print(f.read())
+f.close()
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make 
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+f = open('bar.txt', 'w+')
+for i in range(3):
+    f.write("bar.text created! This is line %d\r\n" % (i+1))
+f.close()
+
+
+# Creating an extra file for practice, not in the assignment
+j = open('jam.txt', "w+")
+j.write('Jamar\'s favorite foods are tacos, mac&cheese, caesar salad, buffalo chicken and pizza.\n')
+j.write('he also needs to exercise a lot to make up for it... \n')
+j.write('maybe even use the nutrition tracker app he helped to create ;) !')
+j.close()
+
+# appending an extra line of text
+j = open('jam.txt', "a+")
+j.write('\n This is an extra line appended to the list of text!')
+print(j.read())
+j.close()
+
+j = open('jam.txt', 'r')
+print(j.read())
+j.close()
