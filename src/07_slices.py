ba780753086162aebdf498a38aa21606f6bba7f1
Jamar Torres <Jetorres88@gmail.com>
2019-11-17 17:29:57 -0500

    completed 09 dictionaries file

--- src/07_slices.py
+++ src/07_slices.py
@@ -11,6 +11,9 @@ Use Python's slice syntax to achieve the following:
 
 a = [2, 4, 1, 7, 9, 6]
 
+# testing out printing the first two elements from the list. 
+# print(a[:2])
+
 # Output the second element: 4:
 print(a[1:2])
 
@@ -18,7 +21,8 @@ print(a[1:2])
 print(a[4:5])
 
 # Output the last three elements in the array: [7, 9, 6]
-print(a[-3:])
+print(a[-3:]) 
+    ## can also do print(a[3:])
 
 # Output the two middle elements in the array: [1, 7]
 print(a[2:4])
--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -17,8 +17,13 @@ print (y)
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = [num**3 for num in range(0,10)]
+y = [num**3 for num in range(10)]
 
+
+print(y)
+
+# tried the lambda alternative to solving the same problem. 
+Y = list(map(lambda num: num**3, range(10)))
 
 print(y)
 
